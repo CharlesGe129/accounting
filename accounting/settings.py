39ed0ccadc02f1e01fe0d145eb05b9fa7069d09b
Charles Ge <char1209ge@gmail.com>
2018-05-11 19:31:24 -0700

    Database models(better not to use)

--- accounting/settings.py
+++ accounting/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'expenses.apps.ExpenseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    id = models.AutoField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+
+class Expenses(models.Model):
+    id = models.AutoField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=255)
+    type_id = models.IntegerField()
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    comment = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    category_id = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
