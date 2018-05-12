66dc4294ec16e2cbeb31de45904ce13f0b4ccd19
Charles Ge <char1209ge@gmail.com>
2018-05-12 14:08:44 -0700

    Implement editing

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'expenses'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:expense_id>/edit/', views.edit, name='edit')
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,19 +1,43 @@
 from django.shortcuts import render
-from .models import Expense
+from .models import Expense, Category
 import datetime
 
 
 def index(request):
     month = datetime.datetime.today().strftime('%Y-%m')
     expenses = Expense.objects.filter(created_at__startswith=month)
-    context = index_form(expenses)
+    context = index_context(expenses)
     return render(request, 'expenses/index.html', context)
 
 
-def index_form(expenses):
+def index_context(expenses):
     sum_expense = 0
     for expense in expenses:
         expense.type = 'Expense' if expense.type_id == 0 else 'Income'
         sum_expense += expense.amount if expense.type_id == 0 else -expense.amount
-    avg_expense = sum_expense / expenses.count()
+        expense.created_at = expense.created_at.strftime('%Y-%m-%d %H:%M:%S')
+    avg_expense = sum_expense / int(datetime.datetime.today().strftime('%m'))
     return {'expenses': expenses, 'sum': sum_expense, 'avg': round(avg_expense, 2)}
+
+
+def edit(request, expense_id):
+    msg = ''
+    if request.method == 'POST':
+        edit_expense(request.POST, expense_id)
+        msg = 'Expense updated successfully'
+    return render(request, 'expenses/edit.html', edit_context(expense_id), msg)
+
+
+def edit_context(expense_id, msg=''):
+    return {'expense': Expense.objects.filter(id=expense_id)[0], 'categories': Category.objects.all(), 'msg': msg}
+
+
+def edit_expense(params, expense_id):
+    expense = Expense.objects.filter(id=expense_id)[0]
+    expense.name = params['name']
+    expense.amount = params['amount']
+    expense.type_id = params['type_id']
+    expense.comment = params['comment']
+    expense.category = Category.objects.filter(id=params['category_id'])[0]
+    expense.save()
+    print(expense.name)
